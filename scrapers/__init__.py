ef5b40e701526a7f62d969aa7dabf68db4165c97
solvcorporate <solvcorporate@gmail.com>
2025-04-18 23:12:05 +0100

    Update __init__.py

--- scrapers/__init__.py
+++ scrapers/__init__.py
@@ -4,5 +4,22 @@ This package contains scrapers for different car listing sites.
 """
 
 from .base import BaseScraper
+from .autotrader import AutoTraderScraper
 
-__all__ = ['BaseScraper']
+__all__ = ['BaseScraper', 'AutoTraderScraper', 'get_scraper']
+
+def get_scraper(scraper_name: str):
+    """Get a scraper instance by name.
+    
+    Args:
+        scraper_name: Name of the scraper to get
+        
+    Returns:
+        Scraper instance or None if not found
+    """
+    scrapers = {
+        "autotrader": AutoTraderScraper(),
+        # Add more scrapers here as they are implemented
+    }
+    
+    return scrapers.get(scraper_name.lower())
